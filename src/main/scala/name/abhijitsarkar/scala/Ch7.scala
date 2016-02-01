582ed420fb7fe2b6537a92130e3a09c08a6618c2
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2016-01-31 21:54:26 -0800

    Ch7 Scaladoc

--- src/main/scala/name/abhijitsarkar/scala/Ch7.scala
+++ src/main/scala/name/abhijitsarkar/scala/Ch7.scala
@@ -1,16 +1,25 @@
 package name.abhijitsarkar.scala
 
+import scala.collection.mutable.Buffer
+
 /**
   * @author Abhijit Sarkar
   */
 object Ch7 {
+  /**
+    * Q7.1a: Write a function that returns a list of the first x elements in the Fibonacci series.
+    * Can you write this with a Buffer? Would a Builder be appropriate here?
+    *
+    * Ans: A Builder is not appropriate here because it does not have a `take` method that I used
+    * for an early exit.
+    */
   def fibonacci(x: Int) = {
     @annotation.tailrec
-    def loop(n: Int, prev: Long, cur: Long, acc: Seq[Long]): Seq[Long] =
+    def loop(n: Int, prev: Long, cur: Long, acc: Buffer[Long]): Buffer[Long] =
       if (n <= 2) acc
-      else loop(n - 1, cur, prev + cur, acc :+ (prev + cur))
+      else loop(n - 1, cur, prev + cur, acc += (prev + cur))
 
-    val acc = Seq[Long](0, 1)
+    val acc = Buffer[Long](0, 1)
 
     val ret = if (x <= 2) acc.take(math.max(x, 0))
     else loop(x, 0, 1, acc)
@@ -18,6 +27,12 @@ object Ch7 {
     ret.toList
   }
 
+  /**
+    * Q7.1c: The `Stream` collection is a great solution for creating a Fibonacci series.
+    * Create a stream that will generate a Fibonacci series.
+    * Use it to print out the first 100 elements in the series,
+    * in a formatted report of 10 comma-delimited elements per line.
+    */
   def fibonacci2(x: Int) = {
     @annotation.tailrec
     def loop(n: Int, prev: Long, cur: Long, acc: Stream[Long]): Stream[Long] =
--- src/main/scala/name/abhijitsarkar/scala/GitHubClient.scala
+++ src/main/scala/name/abhijitsarkar/scala/GitHubClient.scala
@@ -9,7 +9,17 @@ import scala.concurrent.Future
 /**
   * @author Abhijit Sarkar
   */
-
+/**
+  * Q7.6: Write a function that reports recent GitHub commits for a project.
+  * GitHub provides an RSS feed of recent commits for a given user,
+  * repository, and branch, containing XML that you can parse out with regular expressions.
+  * Your function should take the user, repository, and branch, read and parse the RSS feed,
+  * and then print out the commit information.
+  * This should include the date, title, and author of each commit.
+  *
+  * Ans: Instead of parsing XML using regex, which is a bad idea, I used the `spray-json` library
+  * and GitHub REST API to get and parse JSON.
+  */
 trait GitHubClient {
   def commitLogs(url: String) = {
     val commits = concurrent.Future {
